edaa05ac2226877455aed8e40fab07a08a43f4c5
KayLemba <kaylemba@yahoo.com>
2022-01-18 15:02:30 +0200

    Add create and remove book functions

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,20 +3,10 @@ import BookItem from './BookItem';
 import BookAdd from './BookAdd';
 
 function BookList() {
-  const list = [
-    { name: 'firstBook', author: 'kaylemba', id: 1 },
-    { name: 'secondbook', author: 'kaylemba', id: 2 },
-  ];
-  function displayBooks() {
-    const results = list.map((value) => (
-      <BookItem key={value.id} book={value} />
-    ));
-    return results;
-  }
   return (
     <>
       <ul>
-        {displayBooks()}
+        <BookItem />
       </ul>
       <BookAdd />
     </>
